b36a6d23b195891c0c8f0886449e89e841d39adb
Dhruvil <dhruvilmaniar@gmail.com>
2021-12-12 17:30:03 +0530

    Fix cdn http bug

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,7 +11,7 @@ class App extends React.Component {
         super(props);
 
         this.state = {
-            headerText: "Name App",
+            headerText: "Find a Name!",
             headerExpanded: true,
             suggestedNames: [],
         };
